590b7f84a69aaece81e6ba0f4a81ea2aebf89263
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2024-10-26 15:55:30 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Активируем окружение 'sonitr'
+# Activate the 'sonitr' environment
 source /opt/conda/etc/profile.d/conda.sh
 conda activate sonitr
 
-# Запускаем Python-приложение
+# Run the Python application
 python /app/SoniTranslate/app_rvc.py
